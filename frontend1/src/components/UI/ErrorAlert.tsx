fb40559ad3b329f3a5672c126ed688edf8a36c9d
Shashank15DV <sashank@data-vector.com>
2024-09-01 15:38:01 +0530

    initial push

---
+++ frontend1/src/components/UI/ErrorAlert.tsx
@@ -0,0 +1,13 @@
+
+import './ErrorAlert.css';
+
+function ErrorAlert(props : any) {
+  return (
+    <section className='error-alert'>
+      <h2>Something went wrong!</h2>
+      <p>{props.errorText}</p>
+    </section>
+  );
+}
+
+export default ErrorAlert;
